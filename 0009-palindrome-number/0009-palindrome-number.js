cc1e5873724a1f60d983af78ca661a081ed63d4a
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2024-12-30 12:46:54 +0530

    Time: 17 ms (10.6%), Space: 59.2 MB (21.4%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+    const str = x.toString();
+    return str === str.split('').reverse().join('');
+};
