e0ae3698fb78c9bd7cd726a60ae9535698ea6028
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-01-15 20:38:24 +0530

    Time: 14 ms (67.13%), Space: 55 MB (49.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,14 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false
+
+    let charCount: {[key: string]: number} = {}
+    for (let char of s){
+        charCount[char] = (charCount[char] || 0) +1
+    }
+
+    for (let char of t){
+        if (!charCount[char]) return false
+        charCount[char]--
+    }
+    return true
+};
