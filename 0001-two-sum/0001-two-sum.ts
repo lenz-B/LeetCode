485761ab36aecbada7d1985f23248045ea8cc0d5
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-01-16 14:32:49 +0530

    Time: 3 ms (73.75%), Space: 53.4 MB (16.28%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map: Map<number, number> = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        let difference = target - nums[i];
+        if (map.has(difference)) return [map.get(difference), i];
+        map.set(nums[i], i);
+    }
+
+    return [];
+};
