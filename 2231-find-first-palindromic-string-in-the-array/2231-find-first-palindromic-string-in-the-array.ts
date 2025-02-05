b29445813379702882dc1f9852b61cd660dcb2ad
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-02-05 13:06:45 +0530

    Time: 3 ms (70%), Space: 55.3 MB (64.29%) - LeetHub

--- 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.ts
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.ts
@@ -1,6 +1,16 @@
 function firstPalindrome(words: string[]): string {
     for (let word of words){
-        if (word === word.split('').reverse().join('')) return word
+        if (isPalindrome(word)) return word
     }
     return ''
 };
+
+function isPalindrome(word: string): boolean {
+    let left = 0, right = word.length - 1
+    while(left < right){
+        if (word[left] !== word[right]) return false
+        left++
+        right--
+    }
+    return true
+}
