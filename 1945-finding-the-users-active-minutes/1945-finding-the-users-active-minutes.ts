020b1a911ca55927a9599dbb899454b433252b5c
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-03-11 16:55:27 +0530

    Time: 46 ms (41.67%), Space: 75 MB (25%) - LeetHub

--- 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.ts
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.ts
@@ -1,21 +1,19 @@
 function findingUsersActiveMinutes(logs: number[][], k: number): number[] {
-  let obj: {[key: number]: number[]} = {}
+  let obj: {[key: number]: Set<number>} = {}
+  let res = new Array(k).fill(0)
 
   for (let log of logs){
     let username = log[0]
     let activeMin = log[1]
 
-    if(!obj[username]) obj[username] = []
-    if (obj[username] && !obj[username].includes(activeMin)) obj[username].push(activeMin)
+    if(!obj[username]) obj[username] = new Set
+    obj[username].add(activeMin);
   }
 
-  console.log(obj)
-  let lengths = Object.values(obj).map(arr => arr.length)
-  console.log(lengths)
-
-  let res = new Array(k).fill(0)
-  for(let i = 0; i < lengths.length; i++){
-    res[lengths[i] -1]++
+  for (let username in obj){
+    let activeCount = obj[username].size
+    res[activeCount -1]++
   }
+
   return res
 };
