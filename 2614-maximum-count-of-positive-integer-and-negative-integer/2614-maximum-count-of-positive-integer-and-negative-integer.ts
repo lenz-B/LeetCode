d66fac48882b2a6ae122ea2756d44e7e24d1d247
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-03-13 13:11:56 +0530

    Time: 1 ms (38.57%), Space: 58.5 MB (15.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.ts
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.ts
@@ -2,7 +2,7 @@ function maximumCount(nums: number[]): number {
     let pos = 0, neg = 0
     for (let num of nums){
         if (num < 0) neg++
-        if (0 < num) pos++
+        else if (0 < num) pos++
     }
     return pos < neg ? neg : pos 
 };
