84a4833f9e821c6dfb066c31644fcd6d838c1bcf
Muhammed Labeeb <154307607+lenz-B@users.noreply.github.com>
2025-04-22 22:28:00 +0400

    Time: 0 ms (100%), Space: 58.4 MB (47.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,20 +1,15 @@
 function isValid(s: string): boolean {
     const stack: string[] = [];
-    const pairs: { [key: string]: string } = {
+    const pairs: {[key: string] : string} = {
         ')': '(',
         ']': '[',
-        '}': '{',
-    };
-
-    for (const char of s) {
-        if (char in pairs) { // If char is a closing bracket
-            if (stack.pop() !== pairs[char]) {
-                return false;
-            }
-        } else {
-            stack.push(char); // If char is an opening bracket
-        }
+        '}': '{'
     }
 
-    return stack.length === 0;
+    for (let char of s){
+        if (char in pairs) {
+            if (stack.pop() !== pairs[char]) return false
+        } else stack.push(char)
+    }
+    return stack.length === 0
 }
